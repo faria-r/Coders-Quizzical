98353376024d1cc7bd907c99b98e4c9623ecfe35
faria-r <fariacse11@gmail.com>
2022-10-11 10:41:03 +0600

    toast added for showing correct answer

--- src/Components/options/Options.js
+++ src/Components/options/Options.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+const Options = ({option,quiz}) => {
+    const {correctAnswer} = quiz;
 
-const Options = ({option}) => {
+  
+    
+
+    const notify = () => toast("Wow so easy!");
     return (
         <div className='border rounded-xl p-8'>
-            <input type="radio" name="ans" id="" />
+            <input type="radio" name="ans" id={correctAnswer} />
             <span className='ml-2 text-orange-500'>{option}</span>
+            {
+
+            }
         </div>
     );
 };
--- src/Components/quizDetails/QuizDetails.js
+++ src/Components/quizDetails/QuizDetails.js
@@ -1,19 +1,43 @@
+import { faEye, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Options from '../options/Options';
+import {  toast, ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const QuizDetails = ({quiz}) => {
-    console.log(quiz);
-    const {question,options} = quiz;
+    const {question,options,correctAnswer} = quiz;
+
+    // toast for showing correct answer
+    const notify = (ans) => {
+        toast(ans,{
+            autoClose: 2000,
+            closeOnClick: false,
+        })
+        return;
+    };
+
     return (
-        <div className='mt-8 w-3/4 mx-auto'>
+            <div className='mt-8 w-5/6 mx-auto'>
+                       {/* question for quiz  */}
+
             <h2 className='text-indigo-700 font-bold text-xl mb-8 text-center'>{question}</h2>
+            <div>
+            <button onClick={() => notify(correctAnswer)} className='button p-2'>
+            <FontAwesomeIcon className='text-indigo-500 -mr-96 ' icon={faEye}></FontAwesomeIcon>
+            </button>
+            <ToastContainer/>
+                        {/* options for quizes  */}
+
            <div className='grid grid-cols-2 border rounded-xl w-1/2 gap-8 p-6 mx-auto shadow-2xl'>
-           {
-                options.map(option => <Options
+          {
+               options.map(option => <Options
                option={option}
-                ></Options>)
-            }
-           </div>
+               quiz={quiz}
+               ></Options>)
+           }
+            </div>
+            </div>
         </div>
     );
 };
